14b7d47676e020e93b61df2b56ad6bcc70fdad92
dzen100 <tehno700@gmail.com>
2018-05-23 23:55:32 +0300

    reducer and redux commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Root from './components/Root'
+import store from './redux'
+import {Provider} from  'react-redux'
 
 class App extends Component {
   render() {
     return (
-        <div>
-        Hello world!
-        </div>
+        <Provider store = {store}>
+            </Root>
+        </Provider>
     )
   }
 }
